01a6a2e398807871cb0d78697d9563a08b5ddfa8
Trevor Low <tlow94@uw.edu>
2020-04-12 19:08:43 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -20,7 +20,7 @@ def get_english_meaning (path, emoticon)
     if emoticon == value[:japanese]
       return key
     else
-      return "Apology"
+      return "Sorry, that emoticon was not found"
     end
   end
 end
