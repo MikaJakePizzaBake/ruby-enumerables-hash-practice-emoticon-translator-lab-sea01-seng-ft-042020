4b8dd6d2ac4ce4da49e3180c0773ff6191472d00
Trevor Low <tlow94@uw.edu>
2020-04-12 18:54:24 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -2,7 +2,14 @@ require 'yaml'
 require 'pry'
 
 def load_library (path)
-  yaml = YAML.load_file(path)
+  library = YAML.load_file(path)
   new_hash = {}
-  
+  library.each do |key, value|
+    if !library[key]
+      library[key] = {
+        :english => value[0]
+        :japanese => value[1]
+      }
+    end
+  end
 end
